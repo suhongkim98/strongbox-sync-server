6fa795699ba7f8a2e91c81c897c0c88838d0d622
suhongkim98 <kshz9815@naver.com>
2021-02-06 00:30:57 +0900

    stomp 채널 토큰검사 ChannelInterceptor구현

--- src/main/java/com/ssongk/accongbox/config/WebSocketStompConfig.java
+++ src/main/java/com/ssongk/accongbox/config/WebSocketStompConfig.java
@@ -1,15 +1,21 @@
 package com.ssongk.accongbox.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.ChannelRegistration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 
+import com.ssongk.accongbox.interceptor.StompChannelInterceptor;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
 @Configuration
 @EnableWebSocketMessageBroker // stomp 메시지 브로커 활성화
 public class WebSocketStompConfig implements WebSocketMessageBrokerConfigurer {
-
+	private final StompChannelInterceptor stompChannelInterceptor;
 	@Override
 	public void registerStompEndpoints(StompEndpointRegistry registry) {
 		//클라이언트에서 stomp이용한 웹소켓 연결을 할 EndPoint를 설정한다.
@@ -24,5 +30,12 @@ public class WebSocketStompConfig implements WebSocketMessageBrokerConfigurer {
     	registry.enableSimpleBroker("/topic"); // subscribe prefix
     	registry.setApplicationDestinationPrefixes("/app"); // publish 요청 prefix
 	}
+
+	@Override
+	public void configureClientInboundChannel(ChannelRegistration registration) {
+		//토큰검사위한 채널 인터셉터 설정
+		registration.interceptors(stompChannelInterceptor);
+	}
+	
 	
 }
---
+++ src/main/java/com/ssongk/accongbox/interceptor/StompChannelInterceptor.java
@@ -0,0 +1,47 @@
+package com.ssongk.accongbox.interceptor;
+
+import org.springframework.messaging.Message;
+import org.springframework.messaging.MessageChannel;
+import org.springframework.messaging.simp.stomp.StompCommand;
+import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
+import org.springframework.messaging.support.ChannelInterceptor;
+import org.springframework.stereotype.Component;
+
+import com.ssongk.accongbox.exception.CustomJwtRuntimeException;
+import com.ssongk.accongbox.provider.dto.SyncRoom;
+import com.ssongk.accongbox.provider.security.JwtAuthToken;
+import com.ssongk.accongbox.provider.security.JwtAuthTokenProvider;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@Component
+public class StompChannelInterceptor implements ChannelInterceptor {
+//stomp 채널로 들어오는 메시지 가로채서 검사하자
+	
+    private static final String ROOM_ID_KEY = "roomId";
+	private final JwtAuthTokenProvider jwtAuthTokenProvider;
+
+	@Override
+	public Message<?> preSend(Message<?> message, MessageChannel channel) {
+		StompHeaderAccessor accessor = StompHeaderAccessor.wrap(message);
+		
+		if(accessor.getCommand() == StompCommand.SUBSCRIBE) {
+			//구독 시 토큰검사
+			JwtAuthToken token = jwtAuthTokenProvider.convertAuthToken(accessor.getFirstNativeHeader("token")); // 토큰을 헤더에서 꺼내 객체로 만든다.
+			token.validate(); // 토큰 유효성 검사
+			
+			//토큰에 들어가 있는 방정보와 실제 구독하려는 방 정보가 일치하는지 검사
+			String tokenRoomId = "/topic/" + token.getData().get(ROOM_ID_KEY); // 토큰에서 방아이디 꺼내오고
+			if(tokenRoomId.equals(accessor.getDestination()) == false) {
+				//일치하지 않는다면
+				throw new CustomJwtRuntimeException();
+			}
+		} else if (accessor.getCommand() == StompCommand.SEND) {
+			JwtAuthToken token = jwtAuthTokenProvider.convertAuthToken(accessor.getFirstNativeHeader("token")); // 토큰을 헤더에서 꺼내 객체로 만든다.
+			token.validate(); // 토큰 유효성 검사
+		}
+		return message;
+	}
+	
+}
