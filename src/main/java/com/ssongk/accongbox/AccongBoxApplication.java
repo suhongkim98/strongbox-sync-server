ff534d9c59f27dfa62a22f7cfd64501d203b66db
김수홍(Suhong Kim) <35598710+suhongkim98@users.noreply.github.com>
2022-03-26 02:26:34 +0900

    Update AccongBoxApplication.java

--- src/main/java/com/ssongk/accongbox/AccongBoxApplication.java
+++ src/main/java/com/ssongk/accongbox/AccongBoxApplication.java
@@ -7,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class AccongBoxApplication {
 
 	public static void main(String[] args) {
-		System.setProperty("spring.profiles.default", "dev"); // 프로파일을 설정한다. 개발 중에는 dev, 빌드 시에는 prod로
 		SpringApplication.run(AccongBoxApplication.class, args);
 	}
 
